9dc6e79aeeec5756b173d23fed55615886785cf8
Julen Indias <julenindias@gmail.com>
2023-11-27 17:14:07 +0100

    Basic mouse control added
    Mouse dragging and rotation added

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -5,20 +5,55 @@ using UnityEngine.UIElements;
 
 public class ItemController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool dragging = false;
+    public SpriteRenderer spriteRenderer;
+    // offset that will be applied when dragging an item
+    private Vector3 scaleOffset = new Vector3(1.5f, 1.5f, 1.5f);
+    private float scaleTime = 0.3f;
+
+    /// <summary>
+    /// Method for assigning components
+    /// </summary>
+    [ContextMenu("FillComponents")]
+    public void FillComponents()
     {
-        
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        EvaluateDragging();
+    }
+
+    public void EvaluateDragging()
+    {
+        if (dragging)
         {
-            Debug.Log(gameObject.name);
+            spriteRenderer.color = Color.yellow;
+            //spriteRenderer.transform.localScale = Vector3.Scale(Vector3.one, scaleOffset);
+
+            transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+                                             Camera.main.ScreenToWorldPoint(Input.mousePosition).y,
+                                             Camera.main.ScreenToWorldPoint(Input.mousePosition).z + 2f);
+            if (Input.GetMouseButtonDown(1))
+            {
+                Debug.Log("Rigth-clicke: " + transform.rotation.z);
+                //transform.rotation = Quaternion.Euler(0, 0, transform.rotation.z + 90f);
+                transform.Rotate(new Vector3(0, 0, 90));
+            }
         }
     }
 
-    
+    private void OnMouseDown()
+    {
+        dragging = true;
+    }
+
+    private void OnMouseUp()
+    {
+        spriteRenderer.color = Color.white;
+        spriteRenderer.transform.localScale = Vector3.one;
+        dragging = false;
+    }
 }
